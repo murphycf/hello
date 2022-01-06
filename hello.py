8a60041b0218b6c200b86d5c20942b81a0ac0fb5
murphycf@pm.me <murphycf@pm.me>
2022-01-06 07:07:40 -0600

    changes

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+from flask import Flask
 
-name = input("What's your name? ")
-print("Hello" , name)
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
